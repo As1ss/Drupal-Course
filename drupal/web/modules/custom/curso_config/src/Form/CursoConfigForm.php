d105b7262b8bd31b5681bfc4d796db0c1f9c7310
As1ss <alexis.lopbri@educa.jcyl.es>
2024-05-14 11:10:19 +0200

    Proyecto Drupal inyección de servicios clase que hereda de ConfigFormBase

--- drupal/web/modules/custom/curso_config/src/Form/CursoConfigForm.php
+++ drupal/web/modules/custom/curso_config/src/Form/CursoConfigForm.php
@@ -2,15 +2,42 @@
 
 namespace Drupal\curso_config\Form;
 
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\curso_module\services\Repetir;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CursoConfigForm extends ConfigFormBase {
+class CursoConfigForm extends ConfigFormBase
+{
+
+  /** @var Repetir */
+  private Repetir $repetir;
+
+
+  public function __construct(ConfigFactoryInterface $config_factory,Repetir $repetir) {
+    parent::__construct($config_factory);
+    $this->repetir = $repetir;
+  }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  public static function create(ContainerInterface $container)
+  {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('curso_module.repetir')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames()
+  {
     return [
       "curso_config.nuestra_configuracion"
     ];
@@ -19,7 +46,8 @@ class CursoConfigForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId()
+  {
     return "curso_config_nuestro_form";
   }
 
@@ -31,31 +59,29 @@ class CursoConfigForm extends ConfigFormBase {
 
     $form["name"] = [
       "#type" => "textfield",
-      "#title"=> "Name",
-      "#default_value"=> $config->get("name")
+      "#title" => "Name",
+      "#default_value" => $config->get("name")
     ];
 
     $form["label"] = [
       "#type" => "textfield",
-      "#title"=> "Label",
-      "#default_value"=> $config->get("label")
+      "#title" => "Label",
+      "#default_value" => $config->get("label")
     ];
 
-    return parent::buildForm($form,$form_state);
+    return parent::buildForm($form, $form_state);
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
 
-    parent::submitForm($form,$form_state);
+    parent::submitForm($form, $form_state);
 
 
     $config = $this->config("curso_config.nuestra_configuracion");
 
-    $config->set("name",$form_state->getValue("name"));
-    $config->set("label",$form_state->getValue("label"));
+    $config->set("name", $form_state->getValue("name"));
+    $config->set("label", $form_state->getValue("label"));
     $config->save();
   }
-
-
 }
