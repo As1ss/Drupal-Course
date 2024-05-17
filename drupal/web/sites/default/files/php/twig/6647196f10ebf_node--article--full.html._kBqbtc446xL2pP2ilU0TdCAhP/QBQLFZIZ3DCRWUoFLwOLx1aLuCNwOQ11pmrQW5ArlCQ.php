fa36f6488f0d4af7b3758ca49f37d172d0aaf405
As1ss <alexis.lopbri@educa.jcyl.es>
2024-05-17 10:56:45 +0200

    Proyecto Drupal Creación libreria de css js y css extenros

---
+++ drupal/web/sites/default/files/php/twig/6647196f10ebf_node--article--full.html._kBqbtc446xL2pP2ilU0TdCAhP/QBQLFZIZ3DCRWUoFLwOLx1aLuCNwOQ11pmrQW5ArlCQ.php
@@ -0,0 +1,335 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/curso/templates/node--article--full.html.twig */
+class __TwigTemplate_8aa2d1923884801b8b4573b4e6b71d0f extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 69
+        echo "
+";
+        // line 70
+        $context["layout"] = ((($context["layout"] ?? null)) ? (("layout--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["layout"] ?? null), 70, $this->source)))) : (""));
+        // line 71
+        echo "
+";
+        // line 73
+        $context["classes"] = ["node", ("node--type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 75
+($context["node"] ?? null), "bundle", [], "any", false, false, true, 75), 75, $this->source))), ((        // line 76
+($context["layout"] ?? null)) ? ("grid-full") : ("")), ((twig_get_attribute($this->env, $this->source,         // line 77
+($context["node"] ?? null), "isPromoted", [], "method", false, false, true, 77)) ? ("node--promoted") : ("")), ((twig_get_attribute($this->env, $this->source,         // line 78
+($context["node"] ?? null), "isSticky", [], "method", false, false, true, 78)) ? ("node--sticky") : ("")), (( !twig_get_attribute($this->env, $this->source,         // line 79
+($context["node"] ?? null), "isPublished", [], "method", false, false, true, 79)) ? ("node--unpublished") : ("")), ((        // line 80
+($context["view_mode"] ?? null)) ? (("node--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 80, $this->source)))) : (""))];
+        // line 83
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("curso/article"), "html", null, true);
+        echo "
+<article";
+        // line 84
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 84), 84, $this->source), "html", null, true);
+        echo ">
+  <div>Esta es la plantilla full.</div>
+  <header class=\"";
+        // line 86
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["layout"] ?? null), 86, $this->source), "html", null, true);
+        echo "\">
+    ";
+        // line 87
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 87, $this->source), "html", null, true);
+        echo "
+    ";
+        // line 88
+        if ((($context["label"] ?? null) &&  !($context["page"] ?? null))) {
+            // line 89
+            echo "      <h2";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", ["node__title"], "method", false, false, true, 89), 89, $this->source), "html", null, true);
+            echo ">
+        <a href=\"";
+            // line 90
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 90, $this->source), "html", null, true);
+            echo "\" rel=\"bookmark\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 90, $this->source), "html", null, true);
+            echo "</a>
+      </h2>
+    ";
+        }
+        // line 93
+        echo "    ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 93, $this->source), "html", null, true);
+        echo "
+    ";
+        // line 94
+        if (($context["display_submitted"] ?? null)) {
+            // line 95
+            echo "      <div class=\"node__meta\">
+        ";
+            // line 96
+            if (($context["author_picture"] ?? null)) {
+                // line 97
+                echo "          <div class=\"node__author-image\">
+            ";
+                // line 98
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_picture"] ?? null), 98, $this->source), "html", null, true);
+                echo "
+          </div>
+        ";
+            }
+            // line 101
+            echo "        <span";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_attributes"] ?? null), 101, $this->source), "html", null, true);
+            echo ">
+          ";
+            // line 102
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("By"));
+            echo " ";
+            ob_start();
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_name"] ?? null), 102, $this->source), "html", null, true);
+            $___internal_parse_0_ = ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(twig_spaceless($___internal_parse_0_));
+            echo ", ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["date"] ?? null), 102, $this->source), "html", null, true);
+            echo "
+        </span>
+        ";
+            // line 104
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["metadata"] ?? null), 104, $this->source), "html", null, true);
+            echo "
+      </div>
+    ";
+        }
+        // line 107
+        echo "  </header>
+  <div";
+        // line 108
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", ["node__content", ($context["layout"] ?? null)], "method", false, false, true, 108), 108, $this->source), "html", null, true);
+        echo ">
+    ";
+        // line 110
+        echo "    ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter($this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 110, $this->source), "comment"), "html", null, true);
+        echo "
+  </div>
+  ";
+        // line 112
+        if (twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "comment", [], "any", false, false, true, 112)) {
+            // line 113
+            echo "    <div id=\"comments\" class=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["layout"] ?? null), 113, $this->source), "html", null, true);
+            echo "\">
+      ";
+            // line 114
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content"] ?? null), "comment", [], "any", false, false, true, 114), 114, $this->source), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 117
+        echo "</article>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["node", "view_mode", "attributes", "title_prefix", "label", "page", "title_attributes", "url", "title_suffix", "display_submitted", "author_picture", "author_attributes", "author_name", "date", "metadata", "content_attributes", "content"]);    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "themes/custom/curso/templates/node--article--full.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  157 => 117,  151 => 114,  146 => 113,  144 => 112,  138 => 110,  134 => 108,  131 => 107,  125 => 104,  113 => 102,  108 => 101,  102 => 98,  99 => 97,  97 => 96,  94 => 95,  92 => 94,  87 => 93,  79 => 90,  74 => 89,  72 => 88,  68 => 87,  64 => 86,  59 => 84,  55 => 83,  53 => 80,  52 => 79,  51 => 78,  50 => 77,  49 => 76,  48 => 75,  47 => 73,  44 => 71,  42 => 70,  39 => 69,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Olivero's theme implementation to display a node.
+ *
+ * Available variables:
+ * - node: The node entity with limited access to object properties and methods.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - node.getCreatedTime() will return the node creation timestamp.
+ *   - node.hasField('field_example') returns TRUE if the node bundle includes
+ *     field_example. (This does not indicate the presence of a value in this
+ *     field.)
+ *   - node.isPublished() will return whether the node is published or not.
+ *   Calling other methods, such as node.delete(), will result in an exception.
+ *   See \\Drupal\\node\\Entity\\Node for a full list of public properties and
+ *   methods for the node object.
+ * - label: (optional) The title of the node.
+ * - content: All node items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - author_picture: The node author user entity, rendered using the \"compact\"
+ *   view mode.
+ * - metadata: Metadata for this node.
+ * - date: (optional) Themed creation date field.
+ * - author_name: (optional) Themed author name field.
+ * - url: Direct URL of the current node.
+ * - display_submitted: Whether submission information should be displayed.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - node: The current template type (also known as a \"theming hook\").
+ *   - node--type-[type]: The current node type. For example, if the node is an
+ *     \"Article\" it would result in \"node--type-article\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node--view-mode-[view_mode]: The View Mode of the node; for example, a
+ *     teaser would result in: \"node--view-mode-teaser\", and
+ *     full: \"node--view-mode-full\".
+ *   The following are controlled through the node publishing options.
+ *   - node--promoted: Appears on nodes promoted to the front page.
+ *   - node--sticky: Appears on nodes ordered above other non-sticky nodes in
+ *     teaser listings.
+ *   - node--unpublished: Appears on unpublished nodes visible only to site
+ *     admins.
+ * - title_attributes: Same as attributes, except applied to the main title
+ *   tag that appears in the template.
+ * - content_attributes: Same as attributes, except applied to the main
+ *   content tag that appears in the template.
+ * - author_attributes: Same as attributes, except applied to the author of
+ *   the node tag that appears in the template.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ * - view_mode: View mode; for example, \"teaser\" or \"full\".
+ * - teaser: Flag for the teaser state. Will be true if view_mode is 'teaser'.
+ * - page: Flag for the full page state. Will be true if view_mode is 'full'.
+ * - readmore: Flag for more state. Will be true if the teaser content of the
+ *   node cannot hold the main body content.
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_node()
+ */
+#}
+
+{% set layout = layout ? 'layout--' ~ layout|clean_class %}
+
+{%
+  set classes = [
+  'node',
+  'node--type-' ~ node.bundle|clean_class,
+  layout ? 'grid-full',
+  node.isPromoted() ? 'node--promoted',
+  node.isSticky() ? 'node--sticky',
+  not node.isPublished() ? 'node--unpublished',
+  view_mode ? 'node--view-mode-' ~ view_mode|clean_class,
+]
+%}
+{{ attach_library('curso/article') }}
+<article{{ attributes.addClass(classes) }}>
+  <div>Esta es la plantilla full.</div>
+  <header class=\"{{ layout }}\">
+    {{ title_prefix }}
+    {% if label and not page %}
+      <h2{{ title_attributes.addClass('node__title') }}>
+        <a href=\"{{ url }}\" rel=\"bookmark\">{{ label }}</a>
+      </h2>
+    {% endif %}
+    {{ title_suffix }}
+    {% if display_submitted %}
+      <div class=\"node__meta\">
+        {% if author_picture %}
+          <div class=\"node__author-image\">
+            {{ author_picture }}
+          </div>
+        {% endif %}
+        <span{{ author_attributes }}>
+          {{ 'By'|t }} {% apply spaceless %}{{ author_name }}{% endapply %}, {{ date }}
+        </span>
+        {{ metadata }}
+      </div>
+    {% endif %}
+  </header>
+  <div{{ content_attributes.addClass('node__content', layout) }}>
+    {# Comments not part of content, so they won't inherit .text-content styles. #}
+    {{ content|without('comment') }}
+  </div>
+  {% if content.comment %}
+    <div id=\"comments\" class=\"{{ layout }}\">
+      {{ content.comment }}
+    </div>
+  {% endif %}
+</article>
+", "themes/custom/curso/templates/node--article--full.html.twig", "C:\\Users\\alexis.lopez\\workflow\\Drupal-Course\\drupal\\web\\themes\\custom\\curso\\templates\\node--article--full.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 70, "if" => 88, "apply" => 102);
+        static $filters = array("clean_class" => 70, "escape" => 83, "t" => 102, "spaceless" => 102, "without" => 110);
+        static $functions = array("attach_library" => 83);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'apply'],
+                ['clean_class', 'escape', 't', 'spaceless', 'without'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
