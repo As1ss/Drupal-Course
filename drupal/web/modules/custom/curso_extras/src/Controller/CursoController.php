cb3bfc80aa390f88dee176d73f6e0b863e486bfa
As1ss <alexis.lopbri@educa.jcyl.es>
2024-05-20 10:03:14 +0200

    .

--- drupal/web/modules/custom/curso_extras/src/Controller/CursoController.php
+++ drupal/web/modules/custom/curso_extras/src/Controller/CursoController.php
@@ -1,1 +1,17 @@
 <?php
+
+
+
+namespace Drupal\curso_extras\Controller;
+
+class CursoController {
+
+  public function hello(){
+    return [
+      "#theme"=>"curso_controller",
+      "#text"=>"Hello World",
+      "#usuario"=>"Jhon Doe"
+    ];
+  }
+
+}
